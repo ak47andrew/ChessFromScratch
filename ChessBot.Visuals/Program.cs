49fc95be989401a3092d6b706df94853f519f184
ak47andrew <ak47reborntumpa@gmail.com>
2025-03-05 13:32:06 +0300

    Feature: coordinates

--- ChessBot.Visuals/Program.cs
+++ ChessBot.Visuals/Program.cs
@@ -1,33 +1,89 @@
 ﻿using ChessBot.Logic;
 using Raylib_cs;
+using System.Numerics;
 
 namespace ChessBot.Visuals;
 
 class Program {
-    static void DrawBoard(Piece[] pieces){
+    static void DrawBoard(byte[] pieces)
+    {
         int StartposX = (Settings.ScreenWidth - 8 * Settings.CellSize) / 2;
         int StartposY = (Settings.ScreenHeight - 8 * Settings.CellSize) / 2;
 
-        for (int y = 0; y < 8; y++) {
-            for (int x = 0; x < 8; x++) {
+        float fontSize = Settings.FontBaseSize * (Settings.CellSize / 64f); // 64 = base cell size
+        float padding = Settings.CellSize * 0.05f;
+
+        for (int y = 0; y < 8; y++)
+        {
+            for (int x = 0; x < 8; x++)
+            {
                 int posX = StartposX + x * Settings.CellSize;
                 int posY = StartposY + y * Settings.CellSize;
 
-                Raylib.DrawRectangle(posX, posY, Settings.CellSize, Settings.CellSize, 
-                                (x + y) % 2 == 0 ? Settings.CellColorDark : Settings.CellColorLight);
+                // Draw the chessboard cell
+                Color cellColor = (x + y) % 2 == 0 ? Settings.CellColorDark : Settings.CellColorLight;
+                Raylib.DrawRectangle(posX, posY, Settings.CellSize, Settings.CellSize, cellColor);
+
+                // Draw coordinates on the leftmost column (ranks 1-8)
+                if (x == 0)
+                {
+                    int rank = 8 - y;
+                    string rankStr = rank.ToString();
+                    Color textColor = ((x + y) % 2 == 0) ? Settings.CellColorLight : Settings.CellColorDark;
+
+                    Vector2 position = new Vector2(posX + padding, posY + padding);
+                    Raylib.DrawTextPro(
+                        Settings.BoardFont,
+                        rankStr,
+                        position,
+                        Vector2.Zero,
+                        (float)(Math.PI / 2),
+                        fontSize,
+                        0,
+                        textColor
+                    );
+                }
+
+                // Draw coordinates on the bottom row (files a-h)
+                if (y == 7)
+                {
+                    char file = (char)('a' + x);
+                    string fileStr = file.ToString();
+                    Color textColor = ((x + y) % 2 == 0) ? Settings.CellColorLight : Settings.CellColorDark;
+
+                    Vector2 textSize = Raylib.MeasureTextEx(Settings.BoardFont, fileStr, fontSize, 0);
+                    float textX = posX + Settings.CellSize - textSize.X - padding;
+                    float textY = posY + Settings.CellSize - textSize.Y - padding;
+
+                    Raylib.DrawTextEx(
+                        Settings.BoardFont,
+                        fileStr,
+                        new Vector2(textX, textY),
+                        fontSize,
+                        0,
+                        textColor
+                    );
+                }
             }
         }
     }
 
+    static void PreLoad(){
+        Settings.BoardFont = Raylib.LoadFont("resources/NotoSans.ttf");
+        Raylib.SetTextureFilter(Settings.BoardFont.Texture, TextureFilter.Bilinear);
+    }
+
     public static void Main(String[] args){
         Raylib.InitWindow(1280, 720, "ChessBot");
+
+        PreLoad();
 
         while (!Raylib.WindowShouldClose())
         {
             Raylib.BeginDrawing();
             Raylib.ClearBackground(Settings.BackgroundColor);
 
-            DrawBoard(new Piece[1]);
+            DrawBoard(new byte[64]);
 
             Raylib.EndDrawing();
         }
--- ChessBot.Visuals/Settings.cs
+++ ChessBot.Visuals/Settings.cs
@@ -6,8 +6,12 @@ static class Settings {
     public const int ScreenWidth = 1280;
     public const int ScreenHeight = 720;
     public const int CellSize = 85;
+    public const int FontSize = 15;
+    public const int FontBaseSize = 16;
 
     public static readonly Color BackgroundColor = new Color(84, 84, 84);
     public static readonly Color CellColorDark = new Color(118, 150, 86);
     public static readonly Color CellColorLight = new Color(238, 238, 210);
+
+    public static Font BoardFont;
 }
